ac675af08b9c1ae0a504d25b1663f04ac87e6df3
Samuel Mott <samuel.e.mott@gmail.com>
2023-11-29 18:52:11 +0000

    Cleaned up mnemonics

--- src/main.c
+++ src/main.c
@@ -21,7 +21,10 @@ void hexdump(void *buffer, size_t length, size_t base) {
 
 // Linear sweep
 void disassemble_linear(uint8_t *code, size_t length, size_t base) {
-	// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Instruction-Set
+	// References:
+	// - https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Instruction-Set
+	// - http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+
 	if (length % 2 != 0) {
 		fprintf(stderr, "Cannot disassemble code: not aligned to 2 bytes");
 		return;
@@ -33,109 +36,106 @@ void disassemble_linear(uint8_t *code, size_t length, size_t base) {
 		printf("0x%08zx  %04hx    ", base + i, instruction);
 
 		// Parse instruction
-		// TODO: Cleanup mnemonics
 		switch (instruction >> 12) {
 		case 0:
 			switch (0xfff & instruction) {
 			case 0x0E0:
-				printf("clear");
+				printf("CLS");
 				break;
 			case 0x0EE:
-				printf("ret");
+				printf("RET");
 				break;
 			default:
-				printf("callm %#03x", 0xfff & instruction);
+				printf("SYS %#03x", 0xfff & instruction);
 				break;
 			}
 			break;
 		case 1:
-			printf("jmp %#03x", 0xfff & instruction);
+			printf("JMP %#03x", 0xfff & instruction);
 			break;
 		case 2:
-			printf("call %#03x", 0xfff & instruction);
+			printf("CALL %#03x", 0xfff & instruction);
 			break;
 		case 3:
-			printf("skipe v%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
+			printf("SE V%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
 			break;
 		case 4:
-			printf("skipne v%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
+			printf("SNE V%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
 			break;
 		case 5:
-			printf("skipre v%d, v%d", (0xf00 & instruction) >> 8,
+			printf("SE V%d, V%d", (0xf00 & instruction) >> 8,
 			       (0xf0 & instruction) >> 4);
 			break;
 		case 6:
-			printf("store v%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
+			printf("LD V%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
 			break;
 		case 7:
-			printf("add v%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
+			printf("ADD V%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
 			break;
 		case 8:
 			switch (0xf & instruction) {
 			case 0:
-				printf("store v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("LD V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 1:
-				printf("or v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("OR V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 2:
-				printf("and v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("AND V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 3:
-				printf("xor v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("XOR V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 4:
-				// FIX: Does this store the value?
-				printf("add v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("ADD V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 5:
-				// FIX: Does this store the value?
-				printf("sub v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("SUB V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 6:
-				printf("shr v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("SHR V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 7:
-				printf("sub v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("SUBN V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			case 0xE:
-				printf("shl v%d, v%d", (0xf00 & instruction) >> 8,
+				printf("SHL V%d, V%d", (0xf00 & instruction) >> 8,
 				       (0xf0 & instruction) >> 4);
 				break;
 			}
 			break;
 		case 9:
-			printf("skip v%d, v%d", (0xf00 & instruction) >> 8,
+			printf("SNE V%d, V%d", (0xf00 & instruction) >> 8,
 			       (0xf0 & instruction) >> 4);
 			break;
 		case 0xA:
-			printf("store I, %#03x", 0xfff & instruction);
+			printf("LD I, %#03x", 0xfff & instruction);
 			break;
 		case 0xB:
-			printf("jmp0 %#03x", 0xfff & instruction);
+			printf("JMP V0, %#03x", 0xfff & instruction);
 			break;
 		case 0xC:
-			printf("rand v%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
+			printf("RND V%d, %#02x", (0xf00 & instruction) >> 8, 0xff & instruction);
 			break;
 		case 0xD:
-			printf("draw v%d, v%d, %d", (0xf00 & instruction) >> 8,
+			printf("DRW V%d, V%d, %d", (0xf00 & instruction) >> 8,
 			       (0xf0 & instruction) >> 4, 0xf & instruction);
 			break;
 		case 0xE:
 			switch (0xff & instruction) {
 			case 0x9E:
-				printf("skipke v%d", (0xf00 & instruction) >> 8);
+				printf("SKP V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0xA1:
-				printf("skipkne v%d", (0xf00 & instruction) >> 8);
+				printf("SKNP V%d", (0xf00 & instruction) >> 8);
 				break;
 			default:
 				printf("unknown");
@@ -145,31 +145,31 @@ void disassemble_linear(uint8_t *code, size_t length, size_t base) {
 		case 0xF:
 			switch (0xff & instruction) {
 			case 0x07:
-				printf("stored v%d", (0xf00 & instruction) >> 8);
+				printf("LD V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x0A:
-				printf("waitk v%d", (0xf00 & instruction) >> 8);
+				printf("LD V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x15:
-				printf("setd v%d", (0xf00 & instruction) >> 8);
+				printf("LD DT, V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x18:
-				printf("sets v%d", (0xf00 & instruction) >> 8);
+				printf("LD ST, V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x1E:
-				printf("addi v%d", (0xf00 & instruction) >> 8);
+				printf("ADD I, V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x29:
-				printf("seti v%d", (0xf00 & instruction) >> 8);
+				printf("LD F, V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x33:
-				printf("storb v%d", (0xf00 & instruction) >> 8);
+				printf("LD B, V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x55:
-				printf("stori v%d", (0xf00 & instruction) >> 8);
+				printf("LD [I], V%d", (0xf00 & instruction) >> 8);
 				break;
 			case 0x65:
-				printf("fill v%d", (0xf00 & instruction) >> 8);
+				printf("LD V%d, [I]", (0xf00 & instruction) >> 8);
 				break;
 			default:
 				printf("unknown");
