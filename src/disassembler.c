203f46096e56f67cd75a79476ec0ee262e442ed9
Samuel Mott <samuel.e.mott@gmail.com>
2023-12-11 15:31:10 +0000

    Added column offsets to hexdump

--- src/disassembler.c
+++ src/disassembler.c
@@ -14,6 +14,7 @@
 
 void hexdump(void *buffer, size_t length, size_t base) {
 	char ascii[17] = { 0 };
+	printf("Offset    0 1  2 3  4 5  6 7  8 9  A B  C D  E F\n");
 	printf("%08zx: ", base);
 	for (size_t i = 0; i < length; ++i) {
 		char byte = ((char *)buffer)[i];
