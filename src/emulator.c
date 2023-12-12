c29fb95725089c2ae548e82281b461e774aab728
Samuel Mott <samuel.e.mott@gmail.com>
2023-12-12 01:32:51 +0000

    CHIP-8 splash screen rendering working

--- src/emulator.c
+++ src/emulator.c
@@ -57,9 +57,10 @@ SDL_Window *g_window = NULL;
 SDL_Renderer *g_renderer = NULL;
 SDL_Texture *g_texture = NULL;
 
-#define draw(x, y)                                                       \
-	do {                                                             \
-		EMULATOR_DISPLAY[(y) * TARGET_WIDTH + (x)] = 0xFF97F1CD; \
+#define pixel(x, y) EMULATOR_DISPLAY[(y) * TARGET_WIDTH + (x)]
+#define draw(x, y, state)                                  \
+	do {                                               \
+		pixel((x), (y)) ^= state ? 0xFF97F1CD : 0; \
 	} while (false)
 
 void reset_state() {
@@ -105,7 +106,7 @@ bool handle_input() {
 		case SDL_MOUSEMOTION:
 		case SDL_MOUSEBUTTONDOWN:
 			if (e.button.button == SDL_BUTTON_LEFT) {
-				draw(e.motion.x / SCALE_X, e.motion.y / SCALE_Y);
+				draw(e.motion.x / SCALE_X, e.motion.y / SCALE_Y, true);
 			}
 			break;
 		}
@@ -174,37 +175,87 @@ bool next_instruction() {
 			instruction.raw, EMULATOR_PC);
 		return false;
 	case CHIP8_JMP_ADDR:
+		EMULATOR_PC = instruction.aformat.addr;
+		break;
 	case CHIP8_CALL_ADDR:
+		return false;
 	case CHIP8_SE_VX_BYTE:
+		return false;
 	case CHIP8_SNE_VX_BYTE:
+		return false;
 	case CHIP8_SE_VX_VY:
+		return false;
 	case CHIP8_LD_VX_BYTE:
+		EMULATOR_REGISTERS[instruction.iformat.reg] = instruction.iformat.imm;
+		break;
 	case CHIP8_ADD_VX_BYTE:
+		return false;
 	case CHIP8_LD_VX_VY:
+		EMULATOR_REGISTERS[instruction.rformat.rx] =
+			EMULATOR_REGISTERS[instruction.rformat.ry];
+		break;
 	case CHIP8_OR_VX_VY:
+		return false;
 	case CHIP8_AND_VX_VY:
+		return false;
 	case CHIP8_XOR_VX_VY:
+		return false;
 	case CHIP8_ADD_VX_VY:
+		return false;
 	case CHIP8_SUB_VX_VY:
+		return false;
 	case CHIP8_SHR_VX:
+		return false;
 	case CHIP8_SUBN_VX_VY:
+		return false;
 	case CHIP8_SHL_VX:
+		return false;
 	case CHIP8_SNE_VX_VY:
+		return false;
 	case CHIP8_LD_I_ADDR:
+		EMULATOR_VI = instruction.aformat.addr;
+		break;
 	case CHIP8_JMP_V0_ADDR:
+		return false;
 	case CHIP8_RND_VX_BYTE:
-	case CHIP8_DRW_VX_VY_NIBBLE:
+		return false;
+	case CHIP8_DRW_VX_VY_NIBBLE: {
+		int origin_x = EMULATOR_REGISTERS[instruction.rformat.rx];
+		int origin_y = EMULATOR_REGISTERS[instruction.rformat.ry];
+		EMULATOR_REGISTERS[0xF] = pixel(origin_x, origin_y) > 0;
+		for (int row = 0; row < instruction.rformat.imm; ++row) {
+			uint8_t byte = EMULATOR_MEMORY[EMULATOR_VI + row];
+			int y = (origin_y + row) % TARGET_HEIGHT;
+			for (uint8_t col = 0; col < 8 && byte; ++col) {
+				int x = (origin_x + col) % TARGET_WIDTH;
+				draw(x, y, byte & 0x80);
+				byte <<= 1;
+			}
+		}
+		break;
+	}
 	case CHIP8_SKP_VX:
+		return false;
 	case CHIP8_SKNP_VX:
+		return false;
 	case CHIP8_LD_VX_DT:
+		return false;
 	case CHIP8_LD_VX_K:
+		return false;
 	case CHIP8_LD_DT_VX:
+		return false;
 	case CHIP8_LD_ST_VX:
+		return false;
 	case CHIP8_ADD_I_VX:
+		return false;
 	case CHIP8_LD_F_VX:
+		return false;
 	case CHIP8_LD_B_VX:
+		return false;
 	case CHIP8_LD_I_VX:
+		return false;
 	case CHIP8_LD_VX_I:
+		return false;
 	case CHIP8_UNKNOWN:
 		fprintf(stderr, "[!] Unknown instruction received: 0x%04hx @ 0x%03hx\n",
 			instruction.raw, EMULATOR_PC);
